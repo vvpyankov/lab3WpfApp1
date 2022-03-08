b4ed9d4c7771ad3465866f5382e4235205306c4d
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-03-08 12:48:11 +0300

    Добавьте файлы проекта.

---
+++ lab3checkBoxWpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3checkBoxWpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Value is set");
+        }
+
+        private void checkBox1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
